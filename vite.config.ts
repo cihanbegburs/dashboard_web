f3351293d9b30ef859bf7f66654c67a1d05cdccc
Cihan Begburs <cihanbegburs@gmail.com>
2026-06-08 02:26:07 +0300

    allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   server: {
     port: 3001,
     host: '0.0.0.0',
+    allowedHosts: [
+      'dashboard.cihanbegburs.com.tr' // Bu hosta izin veriyoruz
+    ],
     proxy: {
       '^/(auth|finance)': {
         target: 'https://api.cihanbegburs.com.tr', //'localhost:4001',
